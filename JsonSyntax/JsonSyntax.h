de27b124989c803566ba9aa6c041af7f7ea192fa
Oleg Ketrar <oleg.ketrar.dev@icloud.com>
2019-05-20 18:47:04 +0300

    Initial Commit

---
+++ JsonSyntax/JsonSyntax.h
@@ -0,0 +1,19 @@
+//
+//  JsonSyntax.h
+//  JsonSyntax
+//
+//  Created by Oleg Ketrar on 20/05/2019.
+//  Copyright © 2019 Oleg Ketrar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JsonSyntax.
+FOUNDATION_EXPORT double JsonSyntaxVersionNumber;
+
+//! Project version string for JsonSyntax.
+FOUNDATION_EXPORT const unsigned char JsonSyntaxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JsonSyntax/PublicHeader.h>
+
+
